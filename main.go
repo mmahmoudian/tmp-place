a52c9ae04c0dc032d0e61f40f4ffc246ddcaf44b
Mehrad Mahmoudian <m.mahmoudian@gmail.com>
2025-10-31 19:47:04 +0200

    the program generates random 6 character long filename for the uploaded file

--- main.go
+++ main.go
@@ -3,13 +3,28 @@ package main
 import (
 	"fmt"
 	"io"
+	"math/rand"
 	"net/http"
 	"os"
+	"time"
 )
 
-func handler(w http.ResponseWriter, r *http.Request) {
-    // Limit maximum upload size to 10MB
-    r.ParseMultipartForm(10 << 20)
+// GenerateRandomString returns a random alphanumeric string of the given length.
+// If length is zero, it defaults to 6.
+// The generated string includes uppercase letters, lowercase letters, and digits.
+func GenerateRandomString(length ...int) string {
+	const charset = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789"
+	n := 6
+	if len(length) > 0 {
+		n = length[0]
+	}
+	seededRand := rand.New(rand.NewSource(time.Now().UnixNano()))
+	b := make([]byte, n)
+	for i := range b {
+		b[i] = charset[seededRand.Intn(len(charset))]
+	}
+	return string(b)
+}
 
 func handler(cfg Config) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
@@ -20,22 +35,32 @@ func handler(cfg Config) http.HandlerFunc {
 			return
 		}
 
-    // Get the uploaded file
-    file, handler, err := r.FormFile("file")
-    if err == nil {
-        defer file.Close()
-        // Save the uploaded file
-        dst, err := os.Create(handler.Filename)
-        if err != nil {
-            http.Error(w, "Unable to save the file", http.StatusInternalServerError)
-            return
-        }
-        defer dst.Close()
-        io.Copy(dst, file)
-        fmt.Fprintf(w, "File %s uploaded successfully.\n", handler.Filename)
-    } else {
-        fmt.Fprintf(w, "No file uploaded.\n")
-    }
+		name := r.FormValue("name")
+		if name == "" {
+			name = "stranger"
+		}
+		fmt.Fprintf(w, "Hello, %s!\n", name)
+
+		file, handler, err := r.FormFile("file")
+		if err == nil {
+			defer file.Close()
+			// get the original filename
+			originalFilename := handler.Filename
+			// create a new filename with a random tag
+			taggedFilename := fmt.Sprintf("%s_%s", GenerateRandomString(), originalFilename)
+			// save the file to disk using the tagged filename
+			dst, err := os.Create(taggedFilename)
+			if err != nil {
+				http.Error(w, "Unable to save the file", http.StatusInternalServerError)
+				return
+			}
+			defer dst.Close()
+			io.Copy(dst, file)
+			fmt.Fprintf(w, "File %s uploaded successfully.\n", originalFilename)
+		} else {
+			fmt.Fprintf(w, "No file uploaded.\n")
+		}
+	}
 }
 
 func main() {
