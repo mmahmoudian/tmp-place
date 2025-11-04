37f7ad247e014efc72f9addcd3bd6e0e22c2a48b
Mehrad Mahmoudian <m.mahmoudian@gmail.com>
2025-11-04 23:15:40 +0200

    update database schema template based to match the code

--- db_schema.sql
+++ db_schema.sql
@@ -1,20 +1,36 @@
 -- Create "uploads" table
 CREATE TABLE IF NOT EXISTS uploads (
     "index" INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,
+    -- epoch time when the file was uploaded
     upload_epoch INTEGER NOT NULL,
+    -- original filename of the uploaded file
     original_filename TEXT NOT NULL,
+    -- tagged filename (max 10 alphanumeric characters)
     tagged_filename TEXT CHECK(LENGTH(tagged_filename) <= 10 AND tagged_filename GLOB '[0-9A-Za-z]*') NOT NULL,
+    -- file TTL in seconds
     ttl INTEGER UNSIGNED NOT NULL,
+    -- indicate if the download is one-off
     oneoff BOOLEAN NOT NULL,
-    download_secret TEXT CHECK(LENGTH(download_secret) = 40) NOT NULL, -- SHA1 length
-    deletion_epoch INTEGER UNSIGNED
+    -- SHA256 checksum of the download secret
+    download_secret TEXT CHECK(LENGTH(download_secret) = 64),
+    -- Epoch time when the file should be deleted
+    deletion_epoch INTEGER UNSIGNED,
+    -- Size of the uploaded file in bytes
+    file_size INTEGER UNSIGNED NOT NULL,
+    -- SHA1 checksum of the uploaded file
+    file_checksum TEXT CHECK(LENGTH(file_checksum) = 40) NOT NULL,
+    -- indicate if the file has been deleted
+    deleted BOOLEAN NOT NULL DEFAULT 0
 );
 
 -- Create "log" table
 CREATE TABLE IF NOT EXISTS log (
     "index" INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,
-    upload_epoch INTEGER NOT NULL,
-    type TEXT NOT NULL,
+    -- epoch time of the event
+    epoch INTEGER NOT NULL,
+    -- type of the event
+    event_type TEXT NOT NULL,
+    -- log message
     message TEXT
 );
 
