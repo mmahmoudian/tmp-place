20fdbe79ee9ccd28fd4cf6d3020550a5720eb6cf
Mehrad Mahmoudian <m.mahmoudian@gmail.com>
2025-11-10 23:26:28 +0200

    make sure the generated tag names are unique

--- cmd/server/file.go
+++ cmd/server/file.go
@@ -3,6 +3,7 @@ package server
 import (
 	"crypto/sha1"
 	"encoding/hex"
+	"errors"
 	"fmt"
 	"io"
 	"mime/multipart"
@@ -28,15 +29,51 @@ type FileMetadata struct {
 	SHA1Checksum string
 }
 
+// GenerateNewTag generates a unique tagged filename for the uploaded file. It checks if the
+// tag already exists in the database, and make sure the new tag is unique.
+// gets the database file path as input parameter.
+// returns the unique TaggedFilename string.
+func GenerateNewTag(dbFilePath string) (string, error) {
+	// create a new filename with a random tag
+	// ensure the generated TaggedFilename is not already in use, otherwise regenerate a new one
+	var TaggedFilename string
+	for range 100 {
+		TaggedFilename = fmt.Sprint(GenerateRandomTag())
+
+		// query database to make sure TaggedFilename is unique
+		dbQuery := fmt.Sprintf("SELECT COUNT(*) FROM uploads WHERE tagged_filename='%s' AND deleted=false;", TaggedFilename)
+		count, err := shared.QueryOnDatabase(dbFilePath, dbQuery)
+		// fmt.Println("Debug: The count object is:", count)
+		if err != nil {
+			return "", err
+		}
+
+		// extract the value of the count and print it
+		countValue := count[0]["COUNT(*)"]
+
+		// if count is "0", then the TaggedFilename is unique
+		if countValue == 0 {
+			return TaggedFilename, nil
+		}
+	}
+
+	fmt.Println("Alert: Tried 100 times to generate a unique tag, but all existed in database for non-deleted files")
+
+	return "", errors.New("tried 100 times to generate a unique tag, but all existed in database for non-deleted files")
+}
+
 // ReceiveFile is a helper function to handle file saving and metadata storage.
 // It returns the FileInfo struct, and any error encountered.
 func ReceiveFile(file multipart.File, handler *multipart.FileHeader, cfg shared.Config) (FileInfo, error) {
 	// get the original filename
 	originalFilename := handler.Filename
 
-	// create a new filename with a random tag
-	// TODO: ensure the generated TaggedFilename is not already in use, otherwise regenerate a new one
-	TaggedFilename := fmt.Sprint(GenerateRandomTag())
+	// generate a unique tagged filename
+	TaggedFilename, err := GenerateNewTag(cfg.Server.Database.DatabaseFile)
+	if err != nil {
+		// FIXME: the error should be sent to log file and a generic error message returned to user
+		return FileInfo{}, err
+	}
 
 	// save the file to disk using the tagged filename
 	dst, err := os.Create(fmt.Sprintf("%s/%s", cfg.Uploads.Path, TaggedFilename))
